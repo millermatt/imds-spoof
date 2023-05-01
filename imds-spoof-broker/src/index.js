17cce63889a55f46cf507a32360dfcb7b9234d19
Matt Miller <matt.miller@biworldwide.com>
2023-04-30 20:06:02 -0500

    feat: add ability to call external command from broker

--- imds-spoof-broker/src/index.js
+++ imds-spoof-broker/src/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const app = express()
-const pino = require('pino')()
+const bodyParser = require('body-parser')
+const pino = require('pino')({ level: process.env.LOG_LEVEL })
 const pinoHttp = require('pino-http')({ logger: pino, autoLogging: false })
 const { getContainerNameFromIp } = require('./docker')
+const { execSync } = require('child_process');
+
 
 const config = Object.keys(process.env).reduce((accumulator, key) => {
   if (key.startsWith('IMDS_SPOOF_')) {
@@ -21,18 +24,99 @@ function restrictByIp(req, res, next) {
   next()
 }
 
-function addContainerInfo(req, res, next) {
+/*
+this function calls an external command and processes the output of the command
+
+the external command should accept 2 parameters:
+1. name of the docker container that originated the request
+2. a base 64 encoded string, which, when decoded represents the http request. it has these properties:
+    - .method (http method)
+    - .path
+    - .headers
+    - .body (if the request had a body)
+
+the external command should send back a response by printing to stdout. what it prints should
+be a base64 encoded string, which, when decoded results in a json string that has these properties:
+    - .status (http status code)
+    - .headers
+    - .body (optional)
+
+these props will be set on the express js response object sent back to the docker container
+*/
+function broker(req, res, next) {
   const realIp = req.headers['x-real-ip']
   const containerName = getContainerNameFromIp(realIp, config.IMDS_SPOOF_PROXY_DOCKER_NETWORK_NAME)
-  req.dockerContainer = { name: containerName, ip: realIp }
-  req.log.info(JSON.stringify(req.dockerContainer))
-  next()
+
+  // create a request object to send to the external program
+  const partialRequest = {
+    method: req.method,
+    path: req.path,
+    headers: req.headers
+  }
+
+  if (req.body) {
+    partialRequest.body = req.body
+  }
+
+  const base64request = Buffer.from(JSON.stringify(partialRequest)).toString('base64')
+  let output = ''
+
+  try {
+    // Execute the external program with parameters and capture its stdout, stderr, and exit code
+    const output = execSync(`${config.IMDS_SPOOF_BROKER_EXTERNAL_COMMAND} ${containerName} ${base64request}`, { encoding: 'utf8' });
+
+  } catch (error) {
+    // Output the captured stderr and exit code
+    req.log.error(error.stderr);
+    req.log.error(`Exit code: ${error.status}`);
+  }
+
+  // // Output the captured stdout
+  // req.log.debug(output)
+
+  // parse output as json and set similar response props
+  const decodedOutput = decodeBase64(output)
+  if (decodedOutput == null) {
+    req.log.error('Received non-base64 response from external command')
+    res.status(500).send('').end()
+  } else {
+    const externalResponse = JSON.parse(decodedOutput)
+    if (typeof externalResponse.status != 'undefined') {
+      res.status(externalResponse.status)
+    }
+    if (typeof externalResponse.headers != 'undefined') {
+      res.headers = {
+        ...res.headers,
+        ...externalResponse.headers
+      }
+    }
+    if (typeof externalResponse.body != 'undefined') {
+      res.send(externalResponse.body)
+    }
+    res.end()
+  }
+
 }
 
+function decodeBase64(str) {
+  try {
+    // Attempt to decode the string using base64 encoding
+    const decoded = Buffer.from(str, 'base64').toString('utf-8');
+
+    if (decoded == str) {
+      return null
+    }
+    return decoded;
+  } catch (e) {
+    // Catch any errors from attempting to decode the string
+    return null;
+  }
+}
 
 app.use(pinoHttp)
+app.use(bodyParser.text())
 app.use(restrictByIp)
-app.use(addContainerInfo)
+app.use(broker)
 
 const port = config.IMDS_SPOOF_BROKER_PORT,
   host = config.IMDS_SPOOF_BROKER_HOST;
